f716a30f27b5671a07c0de6fecd9ef277284f2bb
Sou 59 <sz5962@gmail.com>
2018-05-18 09:29:04 +0200

    Menu modifié

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,22 @@
-import { Component } from '@angular/core';
-import { Platform, NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { AboutPage } from '../pages/about/about';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
+  @ViewChild(Nav) navCtrl: Nav;
 
   constructor(
     platform: Platform, 
     statusBar: StatusBar, 
-    splashScreen: SplashScreen),
-    public navCtrl: NavController {
+    splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -23,8 +25,9 @@ export class MyApp {
     });
   }
 
-  goToAbout() {
-    this.navCtrl.push(AboutPage);
+  openPage() {
+    this.navCtrl.setRoot(AboutPage);
+    
   }
 
 }
